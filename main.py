b4d2de2e443bb05b1595d567c101c8f9dc8a4d55
dartarus61 <07kirka2002@gmail.com>
2023-09-30 15:55:10 +0300

    fix: save files

--- main.py
+++ main.py
@@ -9,19 +9,19 @@ import os
 sys.path.append('..')
 
 app = FastAPI()
-dir = Path(__file__).parent.parent / 'core' / 'outputData'
+dir = Path(__file__) / 'photoHolder'
  
  
 @app.post("/")
 def read_root(file: UploadFile):
     try:
-        if file.filename.split('.')[1] !='dat':
-            raise Exception('wrong type of file')
         fileName = f'{uuid.uuid4()}_{file.filename}'
         new_file = open(Path(dir / fileName),'wb')
         new_file.write(file.file.read())
         new_file.close()
         fileDir = Path(dir / fileName)
+
+        return fileDir
 
     except Exception as e:
         raise HTTPException(status_code=400, detail=str(e))
