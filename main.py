045798463152e418e130ca0de1a9b4fd90637434
dartarus61 <07kirka2002@gmail.com>
2023-10-01 09:02:50 +0300

    new func

--- main.py
+++ main.py
@@ -35,5 +35,28 @@ def file_contents(files: List[UploadFile]):
             new_file = open(Path(dir / fileName),'wb')
             new_file.write(file.file.read())
             new_file.close()
+
+        return parse_directory('/workspaces/dartar/innoglobalchack-2023/photoHolder')
+        return 'successful'
     except Exception as e:
         raise HTTPException(status_code=400, detail=str(e))
+    
+
+def parse_directory(directory_path):
+    file_links = []
+    
+    # Проверяем, существует ли указанная директория
+    if not os.path.exists(directory_path):
+        print(f"Директория '{directory_path}' не существует.")
+        return file_links
+
+    # Перебираем файлы в директории
+    for root, dirs, files in os.walk(directory_path):
+        for file in files:
+            # Создаем полный путь к файлу
+            file_path = os.path.join(root, file)
+            # Добавляем ссылку на файл в массив
+            file_links.append(file_path)
+    
+    return file_links
+
